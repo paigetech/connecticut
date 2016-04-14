5e9d1a27a953db7b397e072d01604e0f67720112
paigetech <paigetech@gmail.com>
2016-04-14 11:23:08 -0700

    move socket function into angular JS

--- public/js/chat/controllers/ChatController.js
+++ public/js/chat/controllers/ChatController.js
@@ -1,7 +1,56 @@
-app.controller('ChatController', ['$scope', '$http', '$window', function($scope, $http, $window) {
+app.controller('ChatController', ['$scope', '$http', '$window', 'socket', function($scope, $http, $window, socket) {
 
+  $scope.messages = [];
   $scope.submit = function(thing) {
     console.log("angular submit");
   }
+  // Socket listeners
+  // ================
+
+  socket.on('init', function (data) {
+    $scope.name = data.name;
+    $scope.users = data.users;
+  });
+
+  socket.on('chat message', function (message) {
+    $scope.messages.push(message);
+    console.log(" socekt message: " + message);
+  });
+
+
+  // Methods published to the scope
+  // ==============================
+
+
+  $scope.sendMessage = function () {
+    console.log("message: " + $scope.message);
+    socket.emit('chat message', $scope.message );
+
+    // clear message box
+    $scope.message = '';
+  };
 
 }]);
+app.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
